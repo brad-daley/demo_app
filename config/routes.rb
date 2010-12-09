8bf42a0c69eb9e20c69611c27b5c06e39151f9c5
System Administrator <root@devguy10.moveone.info>
2010-12-09 10:32:58 +0100

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 DemoApp::Application.routes.draw do
+  get "sessions/new"
+
   resources :users
+  resources :sessions, :only => [:new, :create, :destory]
 
   match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
